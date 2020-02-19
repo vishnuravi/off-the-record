7a936274e455423132fd24ca70b7b5ff838da104
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-02-19 19:51:34 +0000

    Changes to layout and design

--- offtherecord.py
+++ offtherecord.py
@@ -7,19 +7,24 @@ app = Flask(__name__)
 
 app.config.from_object("config.DevelopmentConfig")
 
-server_url = app.config["SERVER_URL"]
+base_url = app.config["SERVER_URL"]
 sms_number = app.config["SMS_NUMBER"]
 
 client = MongoClient()
 db = client.offtherecord
 
+#make base url and sms number available globally to templates
+@app.context_processor
+def inject_url_and_number():
+    return dict(base_url=base_url, sms_number=sms_number)
+
 #homepage
 @app.route("/")
 def index_page():
-    return render_template('home.html', sms_number=sms_number)
+    return render_template('home.html')
  
 #recieves sms from Twilio and sends back link to compose message
-@app.route("/process_sms", methods=['GET', 'POST'])
+@app.route("/process_sms", methods=['POST'])
 def process_sms():
     phone_number = request.values.get('From', None)
     message = request.values.get('Body', None)
@@ -29,18 +34,20 @@ def process_sms():
     #check if sender's phone number is registered
     valid_user = db.users.find_one({"phone_number" : phone_number})
     if valid_user is not None:
+        #create new token and send compose message link
         token = str(uuid.uuid1())
         db.tokens.insert_one({"token": token, "phone_number": phone_number, "time": int(time.time())})
-        message.body("Hey " + valid_user['first_name'] +"! Tap this link to send a secure message to your doctor. " + server_url + "/c/" + token)
+        message.body("Hey " + valid_user['first_name'] +"! Tap this link to send a secure message to your doctor. " + base_url + "/c/" + token)
     else:
-        message.body("Your number was not recognized as belonging to a current patient. Register at the following link: " + server_url + "/register/" + phone_number)
-    
+        #send user registration link
+        message.body("Your number was not recognized as belonging to a current patient. Register at the following link: " + base_url + "/register/" + phone_number)
     resp.append(message)
     return str(resp)
 
 #renders form to compose message
-@app.route("/c/<token>", methods=['GET', 'POST'])
+@app.route("/c/<token>", methods=['GET'])
 def compose(token):
+    #check if token is valid and render compose message page
     valid_token = db.tokens.find_one({"token": token})
     if valid_token is not None:
         phone_number = valid_token['phone_number']
@@ -51,7 +58,7 @@ def compose(token):
 	
 
 #saves message to database
-@app.route("/send", methods=['GET', 'POST'])
+@app.route("/send", methods=['POST'])
 def save():
     token = request.values.get('token', None)
     valid_token = db.tokens.find_one({"token": token})
@@ -65,19 +72,18 @@ def save():
     return render_template('sent.html')
     
 #register a new phone number
-@app.route("/register/<phone_number>")
+@app.route("/register/<phone_number>", methods=['GET'])
 def register(phone_number):
     return render_template('register.html', phone_number=phone_number)
 	
 #process new registration
-@app.route("/process_registration", methods=['GET', 'POST'])
+@app.route("/create_user", methods=['POST'])
 def process_registration():
     first_name = request.values.get('first_name', None)
     last_name = request.values.get('last_name', None)
     phone_number = request.values.get('phone_number', None)
-    db.users.insert_one({"first_name": first_name, "last_name": last_name, "phone_number": phone_number})
+    db.users.insert_one({"first_name": first_name, "last_name": last_name, "phone_number": phone_number, "verified": "false"})
     return render_template('registered.html')
 
 if __name__ == "__main__":
-    app.debug = True
     app.run(host='0.0.0.0')
---
+++ static/js/blinkingCursor.js
@@ -0,0 +1,12 @@
+var cursor = true;
+var speed = 600;
+
+setInterval(() => {
+  if (cursor) {
+    document.getElementById('cursor').style.opacity = 0;
+    cursor = false;
+  } else {
+    document.getElementById('cursor').style.opacity = 1;
+    cursor = true;
+  }
+}, speed);
